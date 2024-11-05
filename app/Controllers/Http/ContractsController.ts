4818c8a0d6a11f1df08a3fb6fd916603d3610322
DannaMadrid <danna.madrid28691@ucaldas.edu.co>
2024-11-04 22:25:09 -0500

    Creacion contract

---
+++ app/Controllers/Http/ContractsController.ts
@@ -0,0 +1,45 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Contract from "App/Models/Contract";
+
+export default class ContractsController {
+
+        public async find({ request, params }: HttpContextContract) {
+            if (params.id) {
+                let theContract: Contract = await Contract.findOrFail(params.id)
+                return theContract; // Visualizar un solo elemento 
+            } else {
+                const data = request.all()
+                if ("page" in data && "per_page" in data) {
+                    const page = request.input('page', 1); // Paginas 
+                    const perPage = request.input("per_page", 20); // Lista los primeros 20
+                    return await Contract.query().paginate(page, perPage)
+                } else {
+                    return await Contract.query()
+                } // Devuelve todos los elementos 
+    
+            }
+    
+        }
+        public async create({ request }: HttpContextContract) {
+            const body = request.body();
+            const theContract: Contract = await Contract.create(body);
+            return theContract;
+        }
+    
+        public async update({ params, request }: HttpContextContract) {
+            const theContract: Contract = await Contract.findOrFail(params.id);
+            const body = request.body();
+            theContract.description = body.description;
+            theContract.date = body.date;
+            theContract.customer_id = body.customer_id;
+            return await theContract.save();
+        }
+    
+        public async delete({ params, response }: HttpContextContract) {
+            const theContract: Contract = await Contract.findOrFail(params.id);
+                response.status(204);
+                return await theContract.delete();
+        }
+    
+}
---
+++ app/Models/Contract.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Contract extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public description: string
+
+  @column()
+  public date:DateTime
+
+  @column()
+  public customer_id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
