b2472d20a2a9bbe089d071dcf80e10eb0a2b3000
Jeancarlosmurillo <jeanca1229@gmail.com>
2024-12-11 14:29:29 -0500

    .

--- start/routes.ts
+++ start/routes.ts
@@ -1,61 +1,39 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
+import Route from "@ioc:Adonis/Core/Route";
 
-import Route from '@ioc:Adonis/Core/Route'
+Route.get("/", async () => {
+  return { hello: "world" };
+});
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+import "./routes/departaments";
+import "./routes/municipalities";
+import "./routes/addresses";
+import "./routes/distribution_centres";
 
-import './routes/departaments'
-import './routes/municipalities'
-import './routes/addresses'
-import './routes/distribution_centres'
+import "./routes/category"; //registro de la ruta de categorias
+import "./routes/product"; //registro de la ruta de productos
+import "./routes/categoryProducts"; //registro de la ruta de categoria de los productos
+import "./routes/payment"; //registro de la ruta de los pagos
+import "./routes/bill"; //registro de la ruta de las facturas
+import "./routes/customer"; //registro de la ruta de los clientes
+import "./routes/company";
+import "./routes/naturalPerson";
 
-import "./routes/category" //registro de la ruta de categorias 
-import "./routes/product" //registro de la ruta de productos 
-import "./routes/categoryProducts" //registro de la ruta de categoria de los productos 
-import "./routes/payment" //registro de la ruta de los pagos   
-import "./routes/bill" //registro de la ruta de las facturas 
-import "./routes/customer" //registro de la ruta de los clientes 
-import "./routes/company"
-import "./routes/naturalPerson"
-
-import "./routes/contract" //registro de la ruta de contratos
-import "./routes/vehicle" //registro de la ruta de vehiculos
-import "./routes/driver"
-import "./routes/shifts"
-import "./routes/vehiclesdriver"
-import "./routes/sure"
-import "./routes/order"
-import "./routes/route"
-import "./routes/operations"
-import "./routes/restaurants"
-import "./routes/hotels"
-import "./routes/travel_expenses"
-import "./routes/administrators"
-import "./routes/spents"
-import "./routes/owner"
-import "./routes/ownerVehicle"
-import "./routes/services"
-import "./routes/tranches"
-import "./routes/lot"
-
-
+import "./routes/contract"; //registro de la ruta de contratos
+import "./routes/vehicle"; //registro de la ruta de vehiculos
+import "./routes/driver";
+import "./routes/shifts";
+import "./routes/vehiclesdriver";
+import "./routes/sure";
+import "./routes/order";
+import "./routes/route";
+import "./routes/operations";
+import "./routes/restaurants";
+import "./routes/hotels";
+import "./routes/travel_expenses";
+import "./routes/administrators";
+import "./routes/spents";
+import "./routes/owner";
+import "./routes/ownerVehicle";
+import "./routes/services";
+import "./routes/tranches";
+import "./routes/lot";
